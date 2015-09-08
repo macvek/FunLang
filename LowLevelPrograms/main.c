575e14da0fbc018000a0e923171971b3cfbbf942
Maciej Aleksandrowicz <maciejaleksandrowicz@almatron.pl>
2015-09-09 00:08:07 +0200

    Added virtual memory and stack with push and pop operations and a puts wrapper

--- LowLevelPrograms/main.c
+++ LowLevelPrograms/main.c
@@ -7,9 +7,49 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char byte;
+typedef unsigned short memptr;
+
+struct State {
+    byte *stack;
+    byte *memory;
+};
+
+memptr PopPtrFromStack(struct State *state) {
+    state->stack += sizeof(memptr);
+    memptr value = *(memptr*)state->stack;
+    return value;
+}
+
+void PushPtrToStack(struct State *state, memptr ptr) {
+    if (state->stack < state->memory) {
+        exit(1);
+    }
+    memptr *destination = (memptr*)state->stack;
+    *destination = ptr;
+    state->stack -= sizeof(memptr);
+}
+
+void PassToPutS(struct State* state) {
+    char* ptr = (char*)(PopPtrFromStack(state)+state->memory);
+    puts(ptr);
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    struct State state;
+    byte memory[1024];
+    state.stack = memory+sizeof(memory);
+    state.memory = memory;
+    char* string = "HelloWorld\n";
+    int len = strlen(string);
+    memcpy(memory, string, len);
+    
+    PushPtrToStack(&state, 0);
+    PassToPutS(&state);
+    
     return 0;
 }

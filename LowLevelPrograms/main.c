228bd35d96c4b474c37f4d91ee93900f4bf2df99
Maciej Aleksandrowicz <maciejaleksandrowicz@almatron.pl>
2015-09-08 23:00:09 +0200

    Initial Commit

---
+++ LowLevelPrograms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LowLevelPrograms
+//
+//  Created by Maciej Aleksandrowicz on 08.09.2015.
+//  Copyright (c) 2015 macvek. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
